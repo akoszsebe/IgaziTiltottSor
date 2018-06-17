0de0a5edbf5bf386fe640fa04945ec45218316e8
Akos Zsebe <zsebea@yahoo.com>
2018-06-17 20:05:19 +0300

    new service for user management

---
+++ UserService/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Models
+{
+    public class User
+    {
+        public string useremail { get; set; }
+
+        public string address { get; set; }
+
+        public string phone { get; set; }
+
+        public string cardnumber { get; set; }
+
+        public string cardexpiration { get; set; }
+
+        public string cvv { get; set; }
+
+    }
+}
---
+++ UserService/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Models;
+
+namespace UserService.Repositories
+{
+    public interface IUserRepository
+    {
+        bool DoesItemExist(string useremail);
+        IEnumerable<User> All { get; }
+        User Find(string useremail);
+        void Insert(User item);
+        void Update(User item);
+        void Delete(string useremail);
+    }
+}
